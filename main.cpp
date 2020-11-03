8ae0f5bc0663c9c90ed4988076d4e085675458fb
ddiong2828 <62022687+ddiong2828@users.noreply.github.com>
2020-11-03 12:31:23 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,25 @@
 #include <vector>
 #include <functional> 
 #include <stdio.h>
+#include <string.h> 
 #include "word.h"
 
 int main()
 {
     base();
 
-
+    /*char a[100] = "test";
     
     scanf("%s", sen);
 
+    if (strcmp(sen,a) == 0)
+        printf("o");
+    else
+        printf("x");*/
+
+    scanf("%s", sen);
+    
+    
     while (sen[num] != NULL)
     {
         len++;
@@ -30,14 +39,16 @@ int main()
     for (int i = first; i <= last; i++)
         printf("%s\n", _word[i].sen);
 
-    mid = (first + last) / 2;
+    //mid = (first + last) / 2;
 
     printf("%d\n", _word.size());
 
     while (true)
     {
+        /*
         if (sen[len - 1] == _word[mid].sen[len - 1])
             break;
+            */
         mid = (first + last) / 2;
 
         if ((int)sen[1 + ad] < (int)_word[mid].sen[1 + ad])
@@ -46,45 +57,48 @@ int main()
             first = mid;
         else if ((int)sen[1 + ad] == (int)_word[mid].sen[1 + ad])
         {
-            l_num = mid + 2;
-            f_num = mid - 2;
-
-            while(true)
-            {
-                if ((int)sen[2 + ad] == (int)_word[mid].sen[2 + ad])
-                {
-                    l_num = l_num + 2;
-                    continue;
-                }
-
-                l_num = mid - 1;
-                if ((int)sen[2 + ad] == (int)_word[mid].sen[2 + ad])
-                    last = l_num;
-                else
-                    last = l_num - 1;
+            if (strcmp(sen,_word[mid].sen) == 0)
                 break;
-            }
+
+            f_num = mid - 2;
+            l_num = mid + 2;
 
             while (true)
             {
-                if ((int)sen[2 + ad] == (int)_word[mid].sen[2 + ad])
+                if ((int)sen[1 + ad] == (int)_word[f_num].sen[1 + ad])
                 {
-                    f_num = f_num + 2;
+                    f_num = f_num - 2;
                     continue;
                 }
                     
-                f_num = mid + 1;
-                if ((int)sen[2 + ad] == (int)_word[mid].sen[2 + ad])
+                f_num = f_num + 1;
+                if ((int)sen[1 + ad] == (int)_word[f_num].sen[1 + ad])
                     first = f_num;
                 else
                     first = f_num + 1;
                 break;
             }
+
+            while(true)
+            {
+                if ((int)sen[1 + ad] == (int)_word[l_num].sen[1 + ad])
+                {
+                    l_num = l_num + 2;
+                    continue;
+                }
+
+                l_num = l_num - 1;
+                if ((int)sen[1 + ad] == (int)_word[l_num].sen[1 + ad])
+                    last = l_num;
+                else
+                    last = l_num - 1;
+                break;
+            }
+            ad++;
         }
-        ad++;
     }
 
-    printf("%s", _word[mid].sen);
+    printf("%s", _word[mid].mean);
 
     
 
